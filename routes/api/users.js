3a689b310ba88eaa0243a32d6f362f1b4a252060
AndrewBraunsdorf@gmail.com <AndrewBraunsdorf@gmail.com>
2019-07-30 20:35:15 -0400

    added register validation to require name to have between 2 and 30 characters

--- routes/api/users.js
+++ routes/api/users.js
@@ -7,6 +7,9 @@ const jwt = require("jsonwebtoken");
 const keys = require("../../config/keys");
 const passport = require("passport");
 
+// Load Input Validation
+const validateRegisterInput = require("../../validation/register");
+
 // Load User Model
 const User = require("../../models/User");
 
@@ -25,6 +28,13 @@ router.get("/test", (req, res) => res.json({ msg: "Users Works" }));
 // @access  Public
 
 router.post("/register", (req, res) => {
+  const { errors, isValid } = validateRegisterInput(req.body);
+
+  // Check Validation
+  if (!isValid) {
+    return res.status(400).json(errors);
+  }
+
   //use mongoose to first find if the email exists, because we dont want to have somebody register if they have already registered.
   // looking for record of email
   User.findOne({ email: req.body.email }).then(user => {
