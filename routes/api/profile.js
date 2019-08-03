f4397a15dfccfc3640bcb4aa1e7b0ae9bb30bd6f
AndrewBraunsdorf@gmail.com <AndrewBraunsdorf@gmail.com>
2019-08-03 13:18:18 -0400

    Created Post route for Profile to update the profile Fields

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -17,7 +17,7 @@ const User = require("../../models/User");
 // res.json will server json with a status of 200, everything ok.
 router.get("/test", (req, res) => res.json({ msg: "Profile Works" }));
 
-// @route   GET api/profile/profile
+// @route   GET api/profile
 // @desc    Get current user profile
 // @access  Private
 
@@ -41,5 +41,74 @@ router.get(
   }
 );
 
+// @route   Post api/profile
+// @desc    Create or Edit User Profile
+// @access  Private
+
+//since it is a private route we need to add a second input with passport to ensure it is private
+router.post(
+  "/",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    // Get fields
+    // create an empty object which will be filled by things in the form
+    // some dont come from the user
+    const profileFields = {};
+    // req.user.id is the logged in user, include avatar name and email
+    profileFields.user = req.user.id;
+    // check to see if the field has come in
+    // checking if req.body.handle was sent in from the form, if so we are setting it to profileFields.handle
+    if (req.body.handle) profileFields.handle = req.body.handle;
+    if (req.body.company) profileFields.company = req.body.company;
+    if (req.body.website) profileFields.website = req.body.website;
+    if (req.body.location) profileFields.location = req.body.location;
+    if (req.body.bio) profileFields.bio = req.body.bio;
+    if (req.body.status) profileFields.status = req.body.status;
+    if (req.body.githubusername)
+      profileFields.githubusername = req.body.githubusername;
+    // Skills - Split into array
+    // coming in as csv... will give us an array
+    if (typeof req.boody.skills !== "undefined") {
+      profileFields.skills = req.body.skills.split(",");
+    }
+
+    // social fields in its own object
+    //intialize profile fields .socila and set to an empty object
+    profileFields.social = {};
+    if (req.body.youtube) profileFields.social.youtube = req.body.youtube;
+    if (req.body.twitter) profileFields.social.twitter = req.body.twitter;
+    if (req.body.facebook) profileFields.social.facebook = req.body.facebook;
+    if (req.body.linkedin) profileFields.social.linkedin = req.body.linkedin;
+    if (req.body.instagram) profileFields.social.instagram = req.body.instagram;
+
+    // req.user.id is the logged in user
+    Profile.findOne({ user: req.user.id }).then(profile => {
+      // check to see if profile exists
+      if (profile) {
+        // if there is a profile we want to Update it not create a new one
+        //{ $set: profileFields} we want to set the profile fields
+        Profile.findOneAndUpdate(
+          { user: req.user.id },
+          { $set: profileFields },
+          { new: true }
+          // will respond with the profile
+        ).then(profile => res.json(profile));
+      } else {
+        // Create
+
+        //check to see if the handle exists, that way we can access the page in an SCO friendly way
+        Profile.findOne({ handle: profileFields.handle }).then(profile => {
+          if (profile) {
+            errors.handle = "That handle already exists";
+            res.status(400).json(errors);
+          }
+
+          // Save Profile
+          new Profile(profileFields).save().then(profile => res.json(profile));
+        });
+      }
+    });
+  }
+);
 // have to export router in order server.js file to pick it up
 module.exports = router;
