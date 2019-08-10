12404480403e5b89e41f906a13e8c882fe3af169
AndrewBraunsdorf@gmail.com <AndrewBraunsdorf@gmail.com>
2019-08-10 08:13:02 -0400

    added state and onChange to the register page

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,6 +1,25 @@
-import React, { Component } from 'react'
-
+//  have values of state but have to assign change events to add text
+import React, { Component } from 'react';
 class Register extends Component {
+  constructor() {
+    super();
+    // this.state will be an object with some values
+    this.state = {
+      name: '',
+      email: '',
+      password: '',
+      password2: '',
+      errors: {}
+    };
+
+    // Allows you to type.  Since we didnt know what .this was binds this 
+    this.onChange = this.onChange.bind(this);
+  }
+
+  onChange(e) {
+    this.setState({ [e.target.name]: e.target.value });
+  }
+
   render() {
     return (
       <div className="register">
@@ -8,20 +27,46 @@ class Register extends Component {
           <div className="row">
             <div className="col-md-8 m-auto">
               <h1 className="display-4 text-center">Sign Up</h1>
-              <p className="lead text-center">Create your DevConnector account</p>
+              <p className="lead text-center">
+                Create your DevConnector account
+              </p>
               <form action="create-profile.html">
                 <div className="form-group">
-                  <input type="text" className="form-control form-control-lg" placeholder="Name" name="name" required />
+                  <input
+                    type="text"
+                    className="form-control form-control-lg" placeholder="Name"
+                    name="name"
+                    value={this.state.name}
+                    onChange={this.onChange}
+                  />
                 </div>
                 <div className="form-group">
-                  <input type="email" className="form-control form-control-lg" placeholder="Email Address" name="email" />
-                  <small classNameName="form-text text-muted">This site uses Gravatar so if you want a profile image, use a Gravatar email</small>
+                  <input
+                    type="email"
+                    className="form-control form-control-lg" placeholder="Email Address"
+                    name="email"
+                    value={this.state.email}
+                    onChange={this.onChange}
+                  />
+                  <small className="form-text text-muted">This site uses Gravatar so if you want a profile image, use a Gravatar email</small>
                 </div>
                 <div className="form-group">
-                  <input type="password" className="form-control form-control-lg" placeholder="Password" name="password" />
+                  <input
+                    type="password"
+                    className="form-control form-control-lg" placeholder="Password"
+                    name="password"
+                    value={this.state.password}
+                    onChange={this.onChange}
+                  />
                 </div>
                 <div className="form-group">
-                  <input type="password" className="form-control form-control-lg" placeholder="Confirm Password" name="password2" />
+                  <input
+                    type="password"
+                    className="form-control form-control-lg" placeholder="Confirm Password"
+                    name="password2"
+                    value={this.state.password2}
+                    onChange={this.onChange}
+                  />
                 </div>
                 <input type="submit" className="btn btn-info btn-block mt-4" />
               </form>
