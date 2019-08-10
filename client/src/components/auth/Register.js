9249dc77b1679499e4038fecb763bd3d80bbf51b
AndrewBraunsdorf@gmail.com <AndrewBraunsdorf@gmail.com>
2019-08-10 08:21:37 -0400

    added onSubmit function

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -14,10 +14,23 @@ class Register extends Component {
 
     // Allows you to type.  Since we didnt know what .this was binds this 
     this.onChange = this.onChange.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
   }
 
   onChange(e) {
     this.setState({ [e.target.name]: e.target.value });
+  }
+
+  onSubmit(e) {
+    e.preventDefault();
+
+    const newUser = {
+      name: this.state.name,
+      email: this.state.email,
+      password: this.state.password,
+      password: this.state.password2
+    }
+    console.log(newUser);
   }
 
   render() {
@@ -30,7 +43,7 @@ class Register extends Component {
               <p className="lead text-center">
                 Create your DevConnector account
               </p>
-              <form action="create-profile.html">
+              <form onSubmit={this.onSubmit}>
                 <div className="form-group">
                   <input
                     type="text"
