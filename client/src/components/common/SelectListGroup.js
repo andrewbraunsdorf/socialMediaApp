1c7ad78c757f4763d64689df51f008a998de9316
AndrewBraunsdorf@gmail.com <AndrewBraunsdorf@gmail.com>
2019-08-28 21:52:53 -0400

    added SelectListGroup

--- client/src/components/common/SelectListGroup.js
+++ client/src/components/common/SelectListGroup.js
@@ -2,39 +2,44 @@ import React from 'react';
 import classnames from 'classnames';
 import PropTypes from 'prop-types';
 
-const TextAreaFieldGroup = ({
+const SelectListGroup = ({
   name,
-  placeholder,
   value,
   error,
   info,
-  onChange
+  onChange,
+  options
 }) => {
+  const selectionOptions = options.map(option => (
+    <option key={option.label} value={option.value}>
+      {option.label}
+    </option>
+  ))
   return (
     <div className="form-group">
-      <textarea
+      <select
         className={classnames('form-control form-control-lg', {
           'is-invalid': error
         })}
-        placeholder={placeholder}
         name={name}
         value={value}
-        onChange={onChange}
-      />
+        onChange={onChange}>
+        {selectionOptions}
+      </select>
       {info && <small className='form-text text-muted'>{info}</small>}
       {error && <div className="invalid-feedback">{error}</div>}
     </div>
   );
 };
 
-TextAreaFieldGroup.propTypes = {
+SelectListGroup.propTypes = {
   name: PropTypes.string.isRequired,
-  placeholder: PropTypes.string,
   value: PropTypes.string.isRequired,
   info: PropTypes.string,
   error: PropTypes.string,
-  onChange: PropTypes.string.isRequired
+  onChange: PropTypes.string.isRequired,
+  options: PropTypes.array.isRequired
 }
 
 
-export default TextAreaFieldGroup;
+export default SelectListGroup;
