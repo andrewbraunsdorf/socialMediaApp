a68d559265094e15d71bc01c1c2ea9f855257cc0
AndrewBraunsdorf@gmail.com <AndrewBraunsdorf@gmail.com>
2019-09-01 22:38:43 -0400

    added profile handle to front end

--- client/src/components/create-profile/CreateProfile.js
+++ client/src/components/create-profile/CreateProfile.js
@@ -25,10 +25,23 @@ class CreateProfile extends Component {
       youtube: '',
       instagram: '',
       errors: {}
-    }
+    };
+
+    this.onChange = this.onChange.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+  }
+  onSubmit(e) {
+    e.preventDefault();
+
+    console.log('submit');
+  }
+
+  onChange(e) {
+    this.setState({ [e.target.name]: e.target.value });
   }
 
   render() {
+    const { errors } = this.state;
     return (
       <div className="create-profile">
         <div className="container">
@@ -39,6 +52,16 @@ class CreateProfile extends Component {
                 Let's get some information to make your profile stand out
               </p>
               <small className="d-block pb-3">* = required fields</small>
+              <form onSubmit={this.onSubmit}>
+                <TextFieldGroup
+                  placeholder="* Profile Handle"
+                  name="handle"
+                  value={this.state.handle}
+                  onChange={this.onChange}
+                  error={errors.handle}
+                  info="A unique handle for your profile URL.  Your full name, company name, a nickname, etc."
+                />
+              </form>
             </div>
           </div>
         </div>
