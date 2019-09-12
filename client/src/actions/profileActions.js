e447a4208fcc3793e5a5cc40c9e1de994b1c738e
AndrewBraunsdorf@gmail.com <AndrewBraunsdorf@gmail.com>
2019-09-11 20:57:57 -0400

    added axios request

--- client/src/actions/profileActions.js
+++ client/src/actions/profileActions.js
@@ -97,6 +97,25 @@ export const deleteEducation = (id) => dispatch => {
     );
 };
 
+// Get all profiles
+export const getProfiles = () => dispatch => {
+  dispatch(setProfileLoading());
+  axios
+    .get('/api/profile/profile/all')
+    .then(res =>
+      dispatch({
+        type: GET_PROFILES,
+        payload: res.data
+      })
+    )
+    .catch(err =>
+      dispatch({
+        type: GET_PROFILES,
+        payload: null
+      })
+    );
+};
+
 // Delete account & profile
 export const deleteAccount = () => dispatch => {
   if (window.confirm('Are you sure? This can NOT be undone!')) {
