7ab9357fda04432fc5f3ab2ceb71313015d2d852
AndrewBraunsdorf@gmail.com <AndrewBraunsdorf@gmail.com>
2019-09-13 22:09:44 -0400

    created shells for Component Classes

---
+++ client/src/components/profile/ProfileGithub.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+class PorifleGithub extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Profile Github</h1>
+      </div>
+    )
+  }
+}
+export default PorifleGithub;
