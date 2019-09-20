af4ea5e8b39353eff79a88aee52e70102f0eb164
AndrewBraunsdorf@gmail.com <AndrewBraunsdorf@gmail.com>
2019-09-20 11:12:33 -0400

    created PostFeed

--- client/src/actions/postActions.js
+++ client/src/actions/postActions.js
@@ -1,11 +1,13 @@
 import axios from 'axios';
 import {
   ADD_POST,
-  GET_ERRORS
+  GET_ERRORS,
+  GET_POSTS,
+  POST_LOADING
 } from './types';
 
 // Add Post
-export const addPost postData => dispatch => {
+export const addPost = postData => dispatch => {
   axios
     .post('/api/posts', postData)
     .then(res =>
@@ -21,3 +23,27 @@ export const addPost postData => dispatch => {
       })
     );
 };
+
+// GET Posts
+export const getPost = () => dispatch => {
+  dispatch(setPostLoading());
+  axios
+    .get('/api/posts')
+    .then(res =>
+      dispatch({
+        type: GET_POSTS,
+        payload: res.data
+      })
+    )
+    .catch(err =>
+      dispatch({
+        type: GET_POSTS,
+        payload: null
+      })
+    );
+};
+
+//  Set Loading state
+export const setPostLoading = () => {
+  type: POST_LOADING
+}
