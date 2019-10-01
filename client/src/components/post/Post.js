9e377f4ba32e0fb00e9426464d96515db21c2ae9
AndrewBraunsdorf@gmail.com <AndrewBraunsdorf@gmail.com>
2019-10-01 18:11:48 -0400

    show posts when you hit comment

--- client/src/components/post/Post.js
+++ client/src/components/post/Post.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
+import PostItem from '../posts/PostItem';
 import Spinner from '../common/Spinner';
 import { getPost } from '../../actions/postActions';
 
@@ -9,9 +11,31 @@ class Post extends Component {
     this.props.getPost(this.props.match.params.id);
   }
   render() {
+    const { post, loading } = this.props;
+    let postContent;
+
+    if (post === null || loading || Object.keys(post).length === 0) {
+      postContent = <Spinner />
+    } else {
+      postContent = (
+        <div>
+          <PostItem post={post} showActions={false} />
+        </div>
+      );
+    }
+
     return (
-      <div>
-        <h1>POST</h1>
+      <div className="post">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-12">
+              <Link to="/feed" className="btn btn-light mb-3">
+                Back to Feed
+              </Link>
+              {postContent}
+            </div>
+          </div>
+        </div>
       </div>
     )
   }
